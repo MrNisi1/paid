5cddf42bd7f4ed3a28c3d10192c46a75591f0ee2
MrNisi1 <104666224+MrNisi1@users.noreply.github.com>
2022-05-10 19:54:06 +0600

    Create paid.py

---
+++ paid.py
@@ -0,0 +1,19 @@
+import os, platform
+import time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations � Your Device Support Tools\033[1;37>
+    time.sleep(3.5)
+    from M import Main
+    Main()
+elif bit == '32bit':
+    print("\x1b[1;91mOpps Sorry Bro Your Mobile Not Support This Tools😡")
+    time.sleep(3.5)
+    from M import Main
+    Main()
