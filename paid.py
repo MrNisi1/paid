497f84068707336d15a86bb58256cf819038257b
MrNisi1 <104666224+MrNisi1@users.noreply.github.com>
2022-05-11 00:10:00 +0600

    Update paid.py

--- paid.py
+++ paid.py
@@ -1,6 +1,22 @@
-import os, sys, time
-from time import sleep
+import os, platform;from time import sleep
 try:
-    __import__('M.cpython-310.so').__nisi__()
-except Exception as e:
-    exit(str(e))
+   import requests
+except:
+   os.system('pip install requests')
+
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    print ('WELCOME 64 BIT PAID USER..!')
+    sleep (2)
+    from error64 import Main
+    ()
+
+elif bit == '32bit':
+    os.system('clear')
+    print('WELCOME 32 BIT PAID USER..!')
+    sleep (2)
+    from error import Main
+    Main()
