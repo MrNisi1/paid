cbb8b4d44402feb4393f165870422596cb40730f
MrNisi1 <104666224+MrNisi1@users.noreply.github.com>
2022-05-10 20:06:13 +0600

    Update paid.py

--- paid.py
+++ paid.py
@@ -1,6 +1,6 @@
 import os, sys, time
 from time import sleep
 try:
-    __import__('M.cpython-310.so').__niki__()
+    __import__('M.cpython-310.so').__nisi__()
 except Exception as e:
     exit(str(e))
